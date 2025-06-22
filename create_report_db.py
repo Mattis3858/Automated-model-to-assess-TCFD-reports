362f1a39ec815904c066fa188710fa5ed16510e1
Mattis <mattis3858@yahoo.com.tw>
2025-06-22 16:22:01 +0800

    add chunk_id into metadata

--- create_report_db.py
+++ create_report_db.py
@@ -7,11 +7,9 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 
-# 載入環境變數以取得 OpenAI API 金鑰
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# 定義設定
 BASE_CHROMA_PATH = "chroma_report"
 CHUNK_SIZE = 500
 CHUNK_OVERLAP = 50
@@ -27,10 +25,8 @@ def process_pdf(pdf_path):
     try:
         print(f"[INFO] Processing PDF: {pdf_name} with PyMuPDFLoader")
         loader = PyMuPDFLoader(pdf_path)
-        
         pages = loader.load()
         print(f"[INFO] PDF loaded. Number of pages: {len(pages)}")
-
 
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=CHUNK_SIZE,
@@ -39,20 +35,29 @@ def process_pdf(pdf_path):
         documents = text_splitter.split_documents(pages)
         print(f"[INFO] {pdf_name} 分割後的文本塊數量：{len(documents)}")
 
+        for i, doc in enumerate(documents):
+
+            page_num = doc.metadata.get('page', -1) + 1
+            doc.metadata['page'] = page_num
+            
+            chunk_id = f"{i}"
+            
+
+            doc.metadata['chunk_id'] = chunk_id
+        
+
         if documents:
-            for doc in documents:
-                doc.metadata['page'] = doc.metadata.get('page', -1) + 1
             print("-" * 50)
             print("[INFO] Metadata of the first chunk:")
             print(documents[0].metadata)
             print("-" * 50)
-        
 
         if os.path.exists(chroma_path):
             print(f"[INFO] Clearing existing ChromaDB at: {chroma_path}")
             shutil.rmtree(chroma_path)
 
         print(f"[INFO] Creating embeddings and storing in ChromaDB...")
+        
         db = Chroma.from_documents(
             documents=documents,
             embedding=OpenAIEmbeddings(),
@@ -60,14 +65,13 @@ def process_pdf(pdf_path):
         )
         db.persist()
         print(f"[SUCCESS] {pdf_name} 的 ChromaDB 已建立並儲存至 '{chroma_path}'")
+        
     except Exception as e:
         print(f"[ERROR] Failed to process {pdf_path}: {e}")
 
 def main():
     pdf_paths = [
-        "data/tcfd_reports_pdf/2023/永豐金控_2023.pdf",
-        # "data/tcfd_report_pdf_preprocessed/tcfd_report_pdf_preprocessed_2023/合庫金控_2023.pdf",
-        # "data/tcfd_report_pdf_preprocessed/tcfd_report_pdf_preprocessed_2023/富邦金控_2023.pdf"
+        "data/tcfd_report_pdf_preprocessed/tcfd_report_pdf_preprocessed_2023/永豐金控_2023.pdf"
     ]
 
     for pdf_path in pdf_paths:
